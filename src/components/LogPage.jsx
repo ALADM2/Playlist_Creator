b692effe8907d1e4e0d1daa2defc5919465aeee5
ALADM2 <aladm1@student.op.ac.nz>
2023-12-05 13:56:42 +1300

    style login page

--- src/components/LogPage.jsx
+++ src/components/LogPage.jsx
@@ -9,9 +9,16 @@ const LogPage = () => {
     }
 
     return (
-        <div className='login'>
+        <>
+            <div className='login'>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h1 style={{ color: '#228B22', fontFamily: 'Cursive', fontWeight:'bold', fontSize: '36px', margin: 0 }}>🎵 Playlist</h1>
+                    <span style={{ fontSize: '10px', margin: '0 5px' }}></span>
+                    <h1 style={{ color: '#4C218B', fontFamily: 'Cursive', fontWeight:'bold', fontSize: '36px', margin: 0 }}>Creator</h1>
+                </div>
+            </div>
             <button onClick={handleClick}>Log In</button>
-        </div>
+        </>
     )
 }
 
