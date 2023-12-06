7a7025f80e0eb374887a09760f2d4223e6e09f59
ALADM2 <aladm1@student.op.ac.nz>
2023-12-06 14:36:21 +1300

    style login page

--- src/components/LogPage.jsx
+++ src/components/LogPage.jsx
@@ -12,12 +12,12 @@ const LogPage = () => {
         <>
             <div className='login'>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <h1 style={{ color: '#228B22', fontFamily: 'Cursive', fontWeight:'bold', margin: 0 }}>🎵 Playlist</h1>
+                    <h1 style={{ color: '#73BBC9', fontFamily: 'inpact, fantasy', fontStyle: 'oblique', letterSpacing:5, WebkitTextStrokeWidth: 1, WebkitTextStrokeColor: 'black', margin: 0 }}>PLAYLIST</h1>
                     <span style={{ fontSize: '10px', margin: '0 5px' }}></span>
-                    <h1 style={{ color: '#4C218B', fontFamily: 'Cursive', fontWeight:'bold', margin: 0 }}>Creator</h1>
+                    <h1 style={{ color: '#080202', fontFamily: 'Andale Mono, monospace', fontWeight:'bold', margin: 0 }}>🎵 CREATOR</h1>
                 </div>
             </div>
-            <button onClick={handleClick}>Log In</button>
+            <button onClick={handleClick}>LOG IN</button>
         </>
     )
 }
