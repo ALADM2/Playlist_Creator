b8b25c0c2c8e93ccb82fb2312215cd1e632bdc8e
ALADM2 <aladm1@student.op.ac.nz>
2023-12-06 15:13:15 +1300

    style main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -80,7 +80,7 @@ const MainPage = () => {
                     ) : <></>}
                 <DropDown setDevice={setDevice} data={'devices'} />
                 {device && playList ? (
-                <button onClick={handlePlay} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button onClick={handlePlay} type="button">
                         Play new playlist
                 </button>
                 ) : <></>}
