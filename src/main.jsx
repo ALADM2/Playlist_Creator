298cddee40708c0d0603c419d551898cd25e4aa7
ALADM2 <aladm1@student.op.ac.nz>
2023-12-08 11:10:48 +1300

    try google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import { TokenProvider } from './contexts/login.jsx';
-import ReactGA from 'react-ga';
-import { root } from 'postcss';
 
-const TRACKING_ID = 'G-BGCM0MH70E';
-
-function initializeReactGA() {
-  ReactGA.initialize(TRACKING_ID);
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
-
-const Root = () => {
-  useEffect(() => {
-    initializeReactGA();
-  }, []);
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router>
-      <TokenProvider>
-        <App />
-      </TokenProvider>
-    </Router>
-  );
-};
-
-export default root;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <TokenProvider>
+      <App />
+    </TokenProvider>
+  </Router>
+)
