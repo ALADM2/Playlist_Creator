3e6ef2974462008831755fe4a4934d4a575de3d3
ALADM2 <aladm1@student.op.ac.nz>
2023-12-08 15:20:02 +1300

    change info layout

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -48,7 +48,7 @@ const DropDown = (props) => {
     return (
         <div className='dropdown'>
             <label htmlFor="options" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                {props.data == 'topSongs' ? 'Select Song' : 'Select device'}
+                {props.data == 'topSongs' ? 'Select first song for the playlist' : 'Select device'}
             </label>
             <select ref={selectRef} onChange={handleSelect} id="options" className="bg-gray-50 border border-gray-300 text-gray-900 
             text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
